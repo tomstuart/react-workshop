29336d286fc4e89187bff646f548c14d6166bb0a
Tom Stuart <tom@codon.com>
2017-01-12 21:03:25 +0000

    Only mark examples as pending during fake-react run

--- spec/react.spec.js
+++ spec/react.spec.js
@@ -3,11 +3,13 @@
 var implementations = {
   real: {
     React: require('react'),
-    ReactDOM: require('react-dom')
+    ReactDOM: require('react-dom'),
+    pending: function () {}
   },
   fake: {
     React: require('../lib/fake-react'),
-    ReactDOM: require('../lib/fake-react-dom')
+    ReactDOM: require('../lib/fake-react-dom'),
+    pending: pending
   }
 };
 
@@ -15,6 +17,7 @@ Object.keys(implementations).forEach(function (name) {
   var implementation = implementations[name];
   var React = implementation.React;
   var ReactDOM = implementation.ReactDOM;
+  var pending = implementation.pending;
 
   describe('React and ReactDOM (' + name + ' implementation)', function () {
     var container, element;
