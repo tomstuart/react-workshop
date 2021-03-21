5b80531d638d31da3bf691d72a3b65da0b562e57
Tom Stuart <tom@codon.com>
2021-03-21 09:10:31 +0000

    Move toContainHTML custom matcher into Jest setup

--- spec/react.spec.js
+++ spec/react.spec.js
@@ -24,47 +24,6 @@ const fillIn = function (target, value) {
   target.dispatchEvent(change);
 };
 
-var clean = function (node) {
-  var Node = node.ownerDocument.defaultView.Node;
-
-  switch (node.nodeType) {
-    case Node.ELEMENT_NODE:
-      for (var i = node.attributes.length - 1; i >= 0; --i) {
-        var attribute = node.attributes[i];
-        if (attribute.name.startsWith('data-')) {
-          node.removeAttribute(attribute.name);
-        }
-      }
-
-      for (var i = node.childNodes.length - 1; i >= 0; --i) {
-        var child = node.childNodes[i];
-        clean(child);
-      }
-
-      break;
-
-    case Node.COMMENT_NODE:
-      node.remove();
-      break;
-  }
-};
-
-expect.extend({
-  toContainHTML(container, html) {
-    var actual = container.cloneNode(true);
-    clean(actual);
-    actual.normalize();
-
-    var expected = container.cloneNode(true);
-    expected.innerHTML = html;
-
-    return {
-      pass: actual.isEqualNode(expected),
-      message: "Expected '" + expected.innerHTML + "', got '" + actual.innerHTML + "'"
-    };
-  }
-});
-
 // These tests incrementally describe some of the React and ReactDOM API.
 // They run once against the real React and ReactDOM, and then run again
 // against the fake React and ReactDOM implementations in lib/fake-react.js
