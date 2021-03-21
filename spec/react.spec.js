88617e412689b56e61eeea175e9e60fc3bc1b318
Tom Stuart <tom@codon.com>
2021-03-21 09:16:18 +0000

    Move test helpers into Jest setup

--- spec/react.spec.js
+++ spec/react.spec.js
@@ -4,26 +4,6 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 import ReactDOM from 'react-dom';
 
-const clickOn = function (target) {
-  var click = new window.MouseEvent('click', { bubbles: true });
-  target.dispatchEvent(click);
-};
-
-const fillIn = function (target, value) {
-  target.value = value;
-
-  // I can’t work out how to do this in a way that works for both real
-  // and fake React, so I’m just papering over the cracks for now.
-
-  // for real React:
-  var ReactTestUtils = require('react-dom/test-utils');
-  ReactTestUtils.Simulate.change(target);
-
-  // for fake React:
-  var change = new window.Event('change', { bubbles: true });
-  target.dispatchEvent(change);
-};
-
 // These tests incrementally describe some of the React and ReactDOM API.
 // They run once against the real React and ReactDOM, and then run again
 // against the fake React and ReactDOM implementations in lib/fake-react.js
